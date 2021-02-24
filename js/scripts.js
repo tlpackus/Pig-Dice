8f0743645fee78a58ad32ebebc743f9f63a0f380
tlpackus <tlpackus@gmail.com>
2021-02-23 19:49:37 -0800

    add Game.prototype.tallyGameScore(player) Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,22 @@ function Game() {
   this.currentPlayer = 1;
 }
 
-Game.prototype.assignId = function () {
+Game.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
 
-Game.prototype.addPlayer = function (player) {
+Game.prototype.addPlayer = function(player) {
   player.id = this.assignId();
   this.players[player.id] = player;
 }
 
-Game.prototype.roll = function () {
+Game.prototype.roll = function() {
   return Math.floor((Math.random() * 6) + 1);
 }
 
-Game.prototype.winnerCheck = function (player) {
+
+Game.prototype.winnerCheck = function(player) {
   if (player.gameScore >= 100) {
     return true;
   } else {
@@ -26,7 +27,7 @@ Game.prototype.winnerCheck = function (player) {
   }
 }
 
-Game.prototype.tallyTurnScore = function (player) {
+Game.prototype.tallyTurnScore = function(player) {
   let rollValue = this.roll()
   if (rollValue > 1) {
     player.turnScore += rollValue;
@@ -34,9 +35,24 @@ Game.prototype.tallyTurnScore = function (player) {
     player.turnScore = 0;
   }
 }
+Game.prototype.tallyGameScore = function(player) {
+  player.gameScore += player.turnScore;
+  player.turnScore = 0;
+}
 
 function Player(name) {
   this.name = name;
   this.turnScore = 0;
   this.gameScore = 0;
 }
+
+// Pseudo User Interface Logic to facilitate testing -------
+
+let game1 = new Game()
+let player1 = new Player("Michael");
+game1.addPlayer(player1);
+game1.tallyTurnScore(player1);
+console.log(game1);
+// game1.tallyGameScore(player1);
+// console.log(game1);
+console.log(game1.winnerCheck(player1));
